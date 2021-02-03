e71fe1db0081daf64623e220011df7a0d66fef5a
Platypumpkin <34032587+Platypumpkin@users.noreply.github.com>
2021-02-03 11:11:54 +0100

    Update environment.ts
    Ajout de la config firebase aux fichiers environment

--- myApp/src/environments/environment.ts
+++ myApp/src/environments/environment.ts
@@ -4,6 +4,15 @@
 
 export const environment = {
   production: false
+  firebaseConfig:{
+  apiKey: "AIzaSyBuQ1qv1c0wR5yqQaJBUNhetrwk10aY0-0",
+  authDomain: "projet-dev-mobile-fcb0f.firebaseapp.com",
+  projectId: "projet-dev-mobile-fcb0f",
+  storageBucket: "projet-dev-mobile-fcb0f.appspot.com",
+  messagingSenderId: "119788440059",
+  appId: "1:119788440059:web:49dc5adaec93decceea35e",
+  measurementId: "G-SLJ6WGT124"
+  }
 };
 
 /*
