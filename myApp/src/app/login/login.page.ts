c2d301ff354f3501b290abe8deefde9fb16f3cec
Gary Prat <gary73@gmx.fr>
2021-02-24 16:24:40 +0100

    ajout authentification et connexion par la page de login

--- myApp/src/app/login/login.page.ts
+++ myApp/src/app/login/login.page.ts
@@ -1,15 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { AngularFireAuth } from '@angular/fire/auth';
+import { ToastController } from '@ionic/angular';
+import { Router } from '@angular/router';
+import 'firebase/auth';
+import { AuthentService } from 'src/app/services/authent.service';
 
 @Component({
-  selector: 'app-login',
+  // tslint:disable-next-line:component-selector
+  selector: 'todo-list-app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
+  public login: FormGroup;
+  public withEmail: boolean;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor( private formBuilder: FormBuilder,
+               private toastController: ToastController, private route: Router,
+               private auth: AuthentService) {
+    this.withEmail = false;
+    this.login = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', Validators.required],
+    });
   }
 
+
+  async loginForm(){
+    if (this.login.valid) {
+      try {
+        await this.auth.login(this.login.get('email').value,
+        this.login.get('password').value);
+        this.route.navigate(['home']);
+      } catch (e) {
+        const toast = await this.toastController.create({
+          color: 'danger',
+          duration: 2000,
+          message: e.message
+        });
+
+        await toast.present();
+      }
+    }
+  }
 }
---
+++ myApp/src/app/services/authent.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import {AngularFireAuth} from '@angular/fire/auth';
+import {AngularFirestore} from '@angular/fire/firestore';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthentService {
+
+  private user$: BehaviorSubject<firebase.default.User>;
+
+  constructor( private afAuth: AngularFireAuth,
+               private af: AngularFirestore ) {
+    this.user$ = new BehaviorSubject(null);
+    this.afAuth.onAuthStateChanged(user =>
+        this.user$.next(user));
+  }
+
+  getConnectedUser(){
+    return this.user$.asObservable();
+  }
+
+  async login(email: string, password: string) {
+    return await this.afAuth.signInWithEmailAndPassword(email, password);
+  }
+
+  async logout() {
+    return await this.afAuth.signOut();
+  }
+
+  async register(email: string, password: string) {
+    const cred = await this.afAuth.createUserWithEmailAndPassword(
+        email,
+        password
+    );
+
+    await cred.user.sendEmailVerification();
+    return cred.user;
+  }
+}
