31d1bdfc17402aa89e5df7b8bb719d2efcc83218
Antoine Rotival <the.korrigan@laposte.net>
2021-03-22 16:02:31 +0100

    correction problème bibliotheques android

--- myApp/android/app/src/main/java/io/ionic/starter/MainActivity.java
+++ myApp/android/app/src/main/java/io/ionic/starter/MainActivity.java
@@ -4,7 +4,7 @@ import android.os.Bundle;
 
 import com.getcapacitor.BridgeActivity;
 import com.getcapacitor.Plugin;
-//import com.baumblatt.capacitor.firebase.auth.CapacitorFirebaseAuth;
+import com.baumblatt.capacitor.firebase.auth.CapacitorFirebaseAuth;
 
 import com.codetrixstudio.capacitor.GoogleAuth.GoogleAuth;
 
