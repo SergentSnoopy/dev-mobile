0fbf3d999518089812bb3087f11bb0b69e8d05ce
Gary Prat <gary73@gmx.fr>
2021-03-24 00:47:42 +0100

    Readme V1

--- README.md
+++ README.md
@@ -1,9 +1,21 @@
 # dev-mobile
-//TODO faire le readme
-//todo faire le readme
-~ faire les rules et les montrer a la soutenance
-permettre de valider un todo
-enlever le service dtotolist
-ajout validateur
-+ bonus:
-Lecture d'une liste
+Auteurs : Prat Gary et Rotival Antoine
+
+## Fonctionnalités
+- Création d'un compte avec une adresse email
+- Connexion par adresse email
+- Gestion de l'oublie des mots de passe
+- Connexion par android
+- Création d'une liste
+- Suppression d'une liste
+- Création d'un Todo
+- Suppression d'un Todo
+- Validation d'un Todo
+- Ajout d'un utilisateur à une liste
+- Déconexion
+
+## Information IHM
+Pour avoir accés au bouton de supression d'une liste oou d'une todo, il faut slide a gauche.
+
+Pour ajouter une utilisateur à une liste ls faut slide à droite.
+
--- myApp/src/app/services/dbto-list.service.ts
+++ myApp/src/app/services/dbto-list.service.ts
@@ -8,18 +8,6 @@ import {ListService} from './list.service';
 @Injectable({
   providedIn: 'root'
 })
-
-
-
-
-
-// todo Faire ça dans list service plutot qu'ici
-
-
-
-
-
-
 export class DbtoListService {
 
   List = new Subject<List[]>();
@@ -27,23 +15,4 @@ export class DbtoListService {
   constructor(private afs: AngularFirestore, private listserv: ListService) {
 
   }
-
-/*
-  async getList(id : string){
-    const queryObservable = this.List.pipe(
-        switchMap(list =>
-            this.afs.collection('Lists').valueChanges()
-        )
-    );
-
-// subscribe to changes
-    queryObservable.subscribe(queriedItems => {
-      console.log(queriedItems);
-      queriedItems.forEach(n => {this.listserv.addList(new List(n['name'])); });
-      queriedItems.forEach(n => {console.log(n); });
-
-    });
-    this.List.next();
-  }
-*/
 }
